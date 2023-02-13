4166f6fed1658739b467a4cbd99e8bee8a4d6da6
Mohammed Fared Rezk <mrezk.c@saip.gov.sa>
2023-02-13 23:15:27 +0200

    feat products model

---
+++ src/models/product.model.ts
@@ -0,0 +1,56 @@
+import db from '../database'
+
+type Product = {
+  id?: number
+  name: string
+  price: number
+  category?: string
+}
+
+class ProductModelStore {
+  async create(product: Product): Promise<Product> {
+    try {
+      const connection = await db.connect()
+      const sql = `INSERT INTO products (name, price, category) 
+      VALUES ($1, $2, $3) 
+      RETURNING *`
+
+      const result = await connection.query(sql, [product.name, product.price, product.category])
+
+      connection.release()
+
+      return result.rows[0]
+    } catch (error: unknown) {
+      throw new Error(`Unable to create product ${(error as Error).message}`)
+    }
+  }
+
+  async index(): Promise<Product[]> {
+    try {
+      const connection = await db.connect()
+      const sql = 'SELECT * FROM products'
+      const result = await connection.query(sql)
+
+      connection.release()
+      return result.rows
+    } catch (error: unknown) {
+      throw new Error(`Could not products : ${(error as Error).message}`)
+    }
+  }
+
+  async show(id: string): Promise<Product> {
+    try {
+      const connection = await db.connect()
+      // eslint-disable-next-line quotes
+      const sql = `SELECT * FROM users WHERE id = ($1)`
+      const result = await connection.query(sql, [id])
+
+      connection.release()
+      return result.rows[0]
+    } catch (error: unknown) {
+      throw new Error(`Could not get product with this ${id} : ${(error as Error).message}`)
+    }
+  }
+}
+
+export default ProductModelStore
