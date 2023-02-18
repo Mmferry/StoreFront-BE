0142c95db4003d4661a86697f13e3568040e8678
Mohammed Fared Rezk <mrezk.c@saip.gov.sa>
2023-02-18 20:38:31 +0200

    test - product model

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -45,6 +45,35 @@ class ProductModelStore {
       throw new Error(`Could not get product with this ${id} : ${(error as Error).message}`)
     }
   }
+
+  async update(p: Product): Promise<Product> {
+    try {
+      const conn = await db.connect()
+      const sql = `UPDATE products
+      SET name = $1, price = $2
+      WHERE id = ($3) 
+      RETURNING *`
+      const result = await conn.query(sql, [p.name, p.price, p.id])
+
+      conn.release()
+      return result.rows[0]
+    } catch (error: unknown) {
+      throw new Error(`Could not update product error : ${(error as Error).message}`)
+    }
+  }
+
+  async delete(id: string): Promise<Product> {
+    try {
+      const conn = await db.connect()
+      const sql = 'DELETE FROM products WHERE id = ($1)'
+      const result = await conn.query(sql, [id])
+
+      conn.release()
+      return result.rows[0]
+    } catch (error: unknown) {
+      throw new Error(`Unable to delete product error : ${(error as Error).message}`)
+    }
+  }
 }
 
 export default ProductModelStore
---
+++ src/tests/models/product.model.spec.ts
@@ -0,0 +1,89 @@
+import { Product } from '../../interfaces/product.interface'
+import ProductModelStore from '../../models/product.model'
+
+const pStore = new ProductModelStore()
+
+describe('Product model test', () => {
+  let initiateProduct: Product
+
+  const product: Product = {
+    name: 'Iphone 11 pro max',
+    price: 5000,
+    category: 'mobile'
+  }
+
+  const deleteProduct = async (id: string) => {
+    await pStore.delete(id)
+  }
+
+  beforeEach(async () => {
+    initiateProduct = await pStore.create(product)
+  })
+
+  afterEach(async () => {
+    await deleteProduct(initiateProduct.id as unknown as string)
+  })
+
+  it('Should have index method', () => {
+    expect(pStore.index).toBeDefined()
+  })
+
+  it('Should have create method', () => {
+    expect(pStore.create).toBeDefined()
+  })
+
+  it('Should have show method', () => {
+    expect(pStore.show).toBeDefined()
+  })
+
+  it('Should have update method', () => {
+    expect(pStore.update).toBeDefined()
+  })
+
+  it('Should have delete method', () => {
+    expect(pStore.delete).toBeDefined()
+  })
+
+  it('Should create product', async () => {
+    await deleteProduct(initiateProduct.id as unknown as string)
+
+    const createdProduct = await pStore.create(product)
+    expect(createdProduct.name).toBe(initiateProduct.name)
+    expect(createdProduct.price).toBe(initiateProduct.price)
+    expect(createdProduct.category).toBe(initiateProduct.category)
+
+    await deleteProduct(createdProduct.id as unknown as string)
+  })
+
+  it('Should return the product by Id', async () => {
+    expect(initiateProduct.name).toBe(initiateProduct.name)
+    expect(initiateProduct.price).toBe(initiateProduct.price)
+    expect(initiateProduct.category).toBe(initiateProduct.category)
+  })
+
+  it('index method should return a list of products', async () => {
+    const result = await pStore.index()
+    expect(result[0].name).toBe(initiateProduct.name)
+    expect(result[0].price).toBe(initiateProduct.price)
+    expect(result[0].category).toBe(initiateProduct.category)
+  })
+
+  it('Should remove the product by id', async () => {
+    await deleteProduct(initiateProduct.id as unknown as string)
+    expect(initiateProduct.name).toBe(initiateProduct.name)
+    expect(initiateProduct.price).toBe(initiateProduct.price)
+  })
+
+  it('Should update the product', async () => {
+    const updatedProduct = {
+      id: initiateProduct.id,
+      name: 'Samsung Galaxy S3',
+      price: 4500
+    }
+
+    const result = await pStore.update(updatedProduct as unknown as Product)
+
+    expect(result.name).toEqual(updatedProduct.name)
+    expect(result.price).toEqual(updatedProduct.price)
+  })
+})
